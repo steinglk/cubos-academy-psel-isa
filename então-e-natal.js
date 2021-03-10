06a1c41e4cce1ae47083056e101ab1f60d3c6fa9
steinglk <soloqstein@outlook.com>
2021-03-09 21:23:42 -0300

    Adicionado comentários para melhor entendimento do algoritmo

--- então-e-natal.js
+++ então-e-natal.js
@@ -1,6 +1,8 @@
 function solucao(produtos, distanciaRestaurante, cupomDesconto, ehPrimeiraCompra) {
     let i = 0, total = 0;
     let freteg = false;
+    // O laço a seguir percorre a lista recebida como parâmetro, 
+    // e calcula um total parcial de cada produto e aplica o desconto de acordo com a categoria.
     for(i = 0; i < produtos.length; i ++) {
         totalp = 0;
         p1 = produtos[i];
@@ -14,8 +16,11 @@ function solucao(produtos, distanciaRestaurante, cupomDesconto, ehPrimeiraCompra
         else if (p1["categoria"] === "SALGADO") {
             totalp -= (totalp*(10/100));
         }
+        // após o desconto aplicado soma-se o valor ao total.
         total += totalp;
     }
+    // conferir se é a primeira compra do usuario, caso seja true, irá conferir e calcular o desconto.
+    // caso contrário não é acionado nenhum cupom e já vai para calcular o frete.
     if(ehPrimeiraCompra === true){
         if (cupomDesconto === "NATAL10"){
             if(total > 3000) {
@@ -39,9 +44,11 @@ function solucao(produtos, distanciaRestaurante, cupomDesconto, ehPrimeiraCompra
             } 
         }
     }
+    // Conferir se algum cupom de frete gratis foi validado.
     if(freteg === true) {
         console.log(total);
     }
+    // Caso não, calcular o valor do frete e somar ao total.
     else {
         total += (distanciaRestaurante*120);
         console.log(total);
