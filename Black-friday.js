970a1bdb34bd608ed16264b841b1b80f3dfa1ce2
steinglk <soloqstein@outlook.com>
2021-03-10 00:24:38 -0300

    Comentários adicionados para melhor entendimento

--- Black-friday.js
+++ Black-friday.js
@@ -1,5 +1,7 @@
 function solucao(produtos, valorMaximo, avaliacaoMinima) {
     let i = 0, j = 0;
+    // A variavel i servirá para percorrer a lista de produtos
+    // A variavel j servirá como referência de indice da nova lista com produtos que passaram pelas condições
     let valor = produtos[0];
     let arr = [];
     for (i = 0; i < produtos.length; i++) {
