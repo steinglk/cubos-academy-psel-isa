90f32eb5313a1f98754a4c747ffafeebadd20c3d
steinglk <soloqstein@outlook.com>
2021-03-09 21:03:17 -0300

    Comentarios adicionados para melhor entendimento

--- Colisão.js
+++ Colisão.js
@@ -1,11 +1,14 @@
 function solucao(velocidade, aceleracao, distanciaObjeto) {
     let distancia = (-1* velocidade*velocidade)/(2*aceleracao);
+    // conferindo se a distancia que ainda será percorrida É MENOR que a distancia de colisão.
       if (distancia > distanciaObjeto) {
           console.log("COLISAO A FRENTE");
       }
+      // conferindo caso as distancias sejam iguais
       else if (distancia === distanciaObjeto) {
           console.log("NAO ACELERE");
       }
+      // Se as duas condições anteriores retornam falso, sobra apenas este caso.
       else {
           console.log("CAMINHO SEGURO");
       }
